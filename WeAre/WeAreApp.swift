c1ab3f428a4925c0fcfb37524758bca2d9bb5ddf
Seokwon5 <tjrdnjs5@gmail.com>
2023-01-05 17:33:06 +0900

    Initial Commit

---
+++ WeAre/WeAreApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeAreApp.swift
+//  WeAre
+//
+//  Created by 이석원 on 2023/01/05.
+//
+
+import SwiftUI
+
+@main
+struct WeAreApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
